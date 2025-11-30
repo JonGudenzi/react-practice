b96369bfdfad2ea174192be52d38d4851f04034f
Jon Gudenzi <jongudenzi@gmail.com>
2025-11-30 14:53:21 -0500

    input box and count for ul

--- src/InputBot.jsx
+++ src/InputBot.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 
 export default function InputBox() {
 
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
 
   return (
     <div>
       <input value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)} />
+        onChange={(e) => setInputValue(e.target.value)} />
+      <button onClick={() => {
+        if (inputValue.trim() === "") return;
+        setItems(prevItems => [...prevItems, inputValue]);
+        setInputValue("");
+      }
+      }>Add</button>
       <p>You typed: {inputValue}</p>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <p>You have {items.length}</p>
     </div>
   );
 }
