9b4101a39f1e9913e4c55052eb14b48ce46cf2b1
Jon Gudenzi <jongudenzi@gmail.com>
2025-12-01 21:17:10 -0500

    disable no text trimmed in input

--- src/InputBox4.jsx
+++ src/InputBox4.jsx
@@ -5,7 +5,8 @@ const [inputState, setInputState] = useState("");
 const [listItems, setListItems] = useState([]);
 
 function submitHandler() {
-    if (inputState === ""){
+    const inputval = inputState.trim();
+    if (inputval === ""){
         return;
     }
    setListItems(prev => [...prev, inputState]);
@@ -17,7 +18,7 @@ return (
         value={inputState}
         onChange={(e) => setInputState(e.target.value)}></input>
         <button
-        disabled={!inputState}
+        disabled={!inputState.trim()}
          onClick={submitHandler}>Add</button>
         <ul>{listItems.map((item, index) => (
             <li key={index}>{item}</li>
