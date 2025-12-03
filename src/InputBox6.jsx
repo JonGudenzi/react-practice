8311c7d9603d78c491ddeecd2cf749fb32e70726
Jon Gudenzi <jongudenzi@gmail.com>
2025-12-02 22:15:27 -0500

    Delete and edit logic for each li

--- src/InputBox6.jsx
+++ src/InputBox6.jsx
@@ -3,14 +3,29 @@ import { useState } from "react";
 export default function InputBox6() {
     const [inputText, setInputText] = useState("");
     const [listItems, setListItems] = useState([]);
+    const [editingIndex, setEditingIndex] = useState(null);
+    const [editingText, setEditingText] = useState("");
 
-    function handler() {
+    function addHandler() {
         const text = inputText.trim();
         if (text === "") {
             return;
         }
         setListItems(prev => ([...prev, text]));
         setInputText("");
+    }
+
+    function deleteHandler(indexToRemove) {
+        setListItems(prev => prev.filter((item, index) => index !== indexToRemove));
+    }
+
+    function saveHandler() {
+        setListItems(prev =>
+            prev.map((item, index) => 
+            index === editingIndex ? editingText : item)
+        );
+        setEditingIndex(null);
+        setEditingText("");
     }
 
     return (
@@ -20,9 +35,34 @@ export default function InputBox6() {
                 onChange={e => setInputText(e.target.value)} />
             <button
                 disabled={!inputText.trim()}
-                onClick={handler}>Add</button>
+                onClick={addHandler}>Add</button>
             <ul>{listItems.map((item, index) => (
-                <li key={index}>{item}</li>
+                <li key={index}>
+                    {editingIndex === index ? (
+                        <input
+                            value={editingText}
+                            onChange={e => setEditingText(e.target.value)} />
+                    ) : (
+                        item
+                    )}
+
+                    {editingIndex === index ? (
+                        <button onClick={saveHandler}>Save</button>
+                    ) : (
+                        <>
+                            <button onClick={() => {
+                                setEditingIndex(index);
+                                setEditingText(item);
+                            }}>
+                                Edit
+                            </button>
+                            <button onClick={() => deleteHandler(index)}>Delete</button>
+                        </>
+                    )}
+
+                </li>
+
+
             ))}</ul>
         </div>
     )
